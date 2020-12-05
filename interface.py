8d8dd6536d7b374e1c0b18baf31b49b879772c37
Todd Hendricks <hendricks.ta@gmail.com>
2020-12-05 15:30:05 -0800

    if arg 3 is shape, print shape of dataframe

--- interface.py
+++ interface.py
@@ -1,6 +1,13 @@
 import sys
+import pandas as pd
 
-program_command = sys.argv[0]
+program_command = sys.argv[1]
 
 if program_command != "datascope":
     raise SystemExit("{} is not the command to initialize datascope. Please type {} before your other arguments.".format(program_command, "datascope"))
+
+data_file = sys.argv[2]
+user_dataframe = pd.read_csv(data_file)
+if sys.argv[3] == "--shape":
+    print("The dataset has {} rows, and {} columns".format(user_dataframe.shape[0], user_dataframe.shape[1]))
+
