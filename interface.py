3673cc22b0f887f7409685ddb76273ed81e71bb8
Todd Hendricks <hendricks.ta@gmail.com>
2020-12-06 13:05:25 -0800

    add test file with missing data

--- interface.py
+++ interface.py
@@ -10,4 +10,12 @@ data_file = sys.argv[2]
 user_dataframe = pd.read_csv(data_file)
 if sys.argv[3] == "--shape":
     print("The dataset has {} rows, and {} columns".format(user_dataframe.shape[0], user_dataframe.shape[1]))
-
+elif sys.argv[3] == "--cols":
+    for column in user_dataframe.columns:
+        print(column)
+elif sys.argv[3] == "--types":
+    print(user_dataframe.dtypes)
+# elif sys.argv[3] == "--null":
+    
+#     pct = column.isnull().sum() * 100 / len(df)
+#     print("{} : {} percent missing values".format(column, pct))
